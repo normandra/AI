68a6a93b7b465c64fc88a710d68faedfedc9f580
normandra <mahendra.norman@gmail.com>
2016-03-15 21:45:46 +0100

    v1.2
    Write/Overwrite to text. Output will be replaced

--- src/machinelearning/Perceptron.java
+++ src/machinelearning/Perceptron.java
@@ -1,5 +1,6 @@
 package machinelearning;
-import java.util.Scanner;
+
+import java.io.*;
 
 /**
  *
@@ -16,106 +17,117 @@ public class Perceptron {
 	
     public static void main(String args[]){
     	Perceptron p = new Perceptron();
-    	Scanner s = new Scanner(System.in);
+    	
+		String Directory = System.getProperty("user.dir");//get local pathing
     	
     	double wSide = 0,wUmfang = 0,bias = 0;
     	int output;
     	double sidenum,localError,globalError;
     	
     	int iteration = 0;
+    	try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("PerceptronResult.txt", false)))) {
+    			
+	    	
+	    	// Training set-----------------------------------------------------------
+	    	do{
+	    	globalError = 0;
+	        iteration++;
+	    	for(int i = 0;i<100;i++){
+	    		p.GenerateFigure();
+	    		if(side == 3) sidenum = 1; else  sidenum = 0;
+	    		result = p.calculateOutput(theta, wSide,sidenum, umfang, wUmfang, bias);
+	    		if(sidenum == 1) output = 1; else output = 0;
+	    		
+	
+	    		out.println("Current weights");
+	    		out.println("Sides: "+ wSide);
+	    		out.println("Umfang weight: "+ wUmfang);
+	    		out.println("Bias: "+ bias);
+	
+	    		
+	    		out.print("Number of sides: ");
+	    		out.print(side);
+	    		
+	    		
+	    		
+	    		out.println();
+	    		out.print("Umfang: ");
+	    		out.print(umfang);
+	
+	    		out.println();
+	    		if (result == 1){
+	    			out.println("its a triangle");
+	    		}else{
+	    			out.println("Its not a triangle");
+	    		}
+	    		
+	
+	    		out.print("true input value:");
+	    		out.println(output);
+	    		
+	       		out.println("iteration: "+iteration);
+	    		out.println();
+	
+	    		
+	    		localError = output - result;
+	    		globalError += localError*localError;
+	    		
+	    		wSide += learning_rate * localError * sidenum;
+	    		wUmfang += learning_rate * localError * umfang;
+	    		bias += learning_rate * localError;
+	
+	 
+	    		
+	    		
+	    	}
+	    	}while (globalError != 0 && iteration <= max_iter);
+	    	
+	    	out.println("Final Function");
+			out.println(wSide+"x"+ " + " +wUmfang +" + "+ bias);
+	    	
+	    	//Test set----------------------------------------------------------------
+	    	for(int i = 0; i < 10 ; i++){
+	    		p.GenerateFigure();
+	    		if(side == 3) sidenum = 1; else  sidenum = 0;
+	
+	    		out.print("Number of sides: ");
+	    		out.print(side);	
+	    	    		
+	    		out.println();
+	    		out.print("Umfang: ");
+	    		out.print(umfang);
+	
+	    		out.println();
+	    		result = p.calculateOutput(theta, wSide,sidenum, umfang, wUmfang, bias);
+	    		
+	    		if (result == 1){
+	    			out.println("its a triangle");
+	    		}else{
+	    			out.println("Its not a triangle");
+	    		}
+	    		
+	    		//read
+	    		out.print("true input value:");
+	    		if(sidenum == 1) output = 1; else output = 0;
+	    		out.println(output);
+	    		out.println();
+	    	}
+    	}catch (Exception e) {
+		    System.out.println("Eror file not created");
+		}
     	
-    	// Training set-----------------------------------------------------------
-    	do{
-    	globalError = 0;
-        iteration++;
-    	for(int i = 0;i<100;i++){
-    		p.GenerateFigure();
-    		if(side == 3) sidenum = 1; else  sidenum = 0;
-
-    		System.out.println("Current weights");
-    		System.out.println("Sides: "+ wSide);
-    		System.out.println("Umfang weight: "+ wUmfang);
-    		System.out.println("Bias: "+ bias);
-
-    		
-    		System.out.print("Number of sides: ");
-    		System.out.print(side);
-    		
-    		
-    		
-    		System.out.println();
-    		System.out.print("Umfang: ");
-    		System.out.print(umfang);
-
-    		System.out.println();
-    		result = p.calculateOutput(theta, wSide,sidenum, umfang, wUmfang, bias);
-    		if (result == 1){
-    			System.out.println("its a triangle");
-    		}else{
-    			System.out.println("Its not a triangle");
-    		}
-    		
-
-    		
-    		//read
-    		System.out.print("true input value:");
-    		//Automatic solve
-    		
-    		if(sidenum == 1) output = 1; else output = 0;
-    		System.out.println(output);
-    		
-    		// Manual 
-    		//output = Integer.parseInt(s.nextLine());
-    		
-    		localError = output - result;
-    		globalError += localError*localError;
-    		
-    		wSide += learning_rate * localError * sidenum;
-    		wUmfang += learning_rate * localError * umfang;
-    		bias += learning_rate * localError;
-
-    		System.out.println("iteration: "+iteration);
-    		System.out.println();
-    		
-    		
-    	}
-    	}while (globalError != 0 && iteration <= max_iter);
-    	
-    	System.out.println("Final Function");
-		System.out.println(wSide+"x"+ " + " +wUmfang +" + "+ bias);
-    	
-    	//Test set----------------------------------------------------------------
-    	for(int i = 0; i < 10 ; i++){
-    		p.GenerateFigure();
-    		if(side == 3) sidenum = 1; else  sidenum = 0;
-
-    		System.out.print("Number of sides: ");
-    		System.out.print(side);	
-    	    		
-    		System.out.println();
-    		System.out.print("Umfang: ");
-    		System.out.print(umfang);
-
-    		System.out.println();
-    		result = p.calculateOutput(theta, wSide,sidenum, umfang, wUmfang, bias);
-    		
-    		if (result == 1){
-    			System.out.println("its a triangle");
-    		}else{
-    			System.out.println("Its not a triangle");
-    		}
-    		
-    		//read
-    		System.out.print("true input value:");
-    		if(sidenum == 1) output = 1; else output = 0;
-    		System.out.println(output);
-    		System.out.println();
-    	}
+    	try
+		{
+		     Runtime.getRuntime().exec("notepad "+ Directory +"\\PerceptronResult.txt");
+		} 
+		catch (Exception ex){
+		     System.out.println("Error opening file");
+		}
     }
     
     private void GenerateFigure(){
     	side = 3 + (int)(Math.random() * ((4 - 3) + 1));
-    	umfang = 30 + (int)(Math.random() * ((1000 - 30) + 1));
+    	umfang = 30 + (int)(Math.random() * ((100 - 30) + 1));
     }
     
     private int calculateOutput(int theta, double wSide, double sidenum , double umfang, double wUmfang, double bias){
